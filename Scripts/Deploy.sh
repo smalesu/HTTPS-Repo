adcbbf6493f2060357f281f1ff9f1f58382ac5bb
smalesu <89542248+smalesu@users.noreply.github.com>
2021-09-21 17:44:09 +0530

    Update Deploy.sh

--- Scripts/Deploy.sh
+++ Scripts/Deploy.sh
@@ -7,7 +7,7 @@ echo /u01/Jenkins_scripts/Properties/$ENV_NAME.properties
 source /u01/Jenkins_scripts/Properties/$ENV_NAME.properties
 
 echo "Copying java-tomcat-maven-example.war from Master machine to /u01/apache-tomcat-8.5.71/webapps"
-sshpass -p ${Password} scp -r /var/lib/jenkins/workspace/Deployment/target/java-tomcat-maven-example.war ${Username}@${IP_Address}:/u01/apache-tomcat-8.5.71/webapps
+sshpass -p ${Password} -o StrictHostKeyChecking=no scp -r /var/lib/jenkins/workspace/Deployment/target/java-tomcat-maven-example.war ${Username}@${IP_Address}:/u01/apache-tomcat-8.5.71/webapps
 
 echo "Connecting to Target machine via SSH"
 sshpass -p ${Password} ssh -t -t ${Username}@${IP_Address} <<EOSPL
